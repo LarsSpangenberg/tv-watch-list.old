4e49b0211ce5951db48a03962d436539678c7cf1
TheGerman8or <Lazerus88@gmail.com>
2018-07-26 00:35:28 -0400

    boilerplate code for client.js, index.html, and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var srcPath = path.join(__dirname, 'src');
+var buildPath = path.join(__dirname, 'dist');
+
+module.exports = {
+  context: srcPath,
+  entry: path.join(srcPath, 'js', 'client.js'),
+  output: {
+    path: buildPath,
+    fileName: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env']
+          }
+        }
+      }
+    ]
+  }
+};
