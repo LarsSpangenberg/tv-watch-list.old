fefb6f284a0bf0e2f8fb0c612aacbb7c5cfe3b87
TheGerman8or <Lazerus88@gmail.com>
2018-08-07 09:48:27 -0400

    css loader and implemented css modules

--- webpack.config.js
+++ webpack.config.js
@@ -39,11 +39,24 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'react']
-          }
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   }
