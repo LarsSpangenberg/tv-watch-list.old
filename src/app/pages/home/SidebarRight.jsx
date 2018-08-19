0e1c1d9b7d0b1388a349346d73284e12c16982ad
TheGerman8or <Lazerus88@gmail.com>
2018-08-18 22:46:31 -0400

    right sidebar hide/show on hover

--- src/app/pages/home/SidebarRight.jsx
+++ src/app/pages/home/SidebarRight.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-
 import styles from './SidebarRight.scss';
 
 
-function SidebarRight(props) {
-  return (
-    <aside className={styles.sidebar}>
-      <div className={styles.icons}>
-        <i className={`fas fa-sort-alpha-down ${styles.icon}`} />
-        <i className={`fas fa-th-list ${styles.icon}`} />
-        <i className={`far fa-calendar-alt ${styles.icon}`} />
-        <i className={`fas fa-cogs ${styles.icon}`} />
-      </div>
-    </aside>
-  );
-}
+export default class SidebarRight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+    };
+    this.handleActive = this.handleActive.bind(this);
+  }
 
-export default SidebarRight;
+  handleActive(e) {
+    const { isActive } = this.state;
+    e.preventDefault();
+    this.setState({ isActive: !isActive });
+  }
+
+  render() {
+    const { isActive } = this.state;
+    return (
+      <aside className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
+
+        <div className={styles.hoverArea} />
+        <div className={styles.pulloutInner}>
+          <div className={styles.icons}>
+            <i className={`fas fa-sort-alpha-down ${styles.icon}`} />
+            <i className={`fas fa-th-list ${styles.icon}`} />
+            <i className={`far fa-calendar-alt ${styles.icon}`} />
+            <i className={`fas fa-cogs ${styles.icon}`} />
+          </div>
+        </div>
+      </aside>
+    );
+  }
+}
