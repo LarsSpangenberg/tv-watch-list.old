1d560acfaf9686736e9c2c548bbf05e8703d29e5
TheGerman8or <Lazerus88@gmail.com>
2018-08-19 15:15:30 -0400

    created a mock user json in assets

--- src/app/pages/home/MainContent.jsx
+++ src/app/pages/home/MainContent.jsx
@@ -17,15 +17,37 @@ function MainContent(props) {
           </tr>
         </thead>
         <tbody>
+
           <tr>
             <td className="thumbnail">
               <i className="far fa-image" />
             </td>
-            <td className="title">Jessica Jones</td>
-            <td className="season">2</td>
-            <td className="episode">13/13</td>
-            <td className="comment">finished the latest season</td>
+            <td className="title">
+              <input
+                type="text"
+                value="Jessica Jones"
+              />
+            </td>
+            <td className="season">
+              <input
+                type="number"
+                value="2"
+              />
+            </td>
+            <td className="episode">
+              <input
+                type="number"
+                value="13"
+              /> / 13
+            </td>
+            <td className="comment">
+              <input
+                type="text"
+                value="finished last season"
+              />
+            </td>
           </tr>
+
           <tr>
             <td className="thumbnail">
               <i className="far fa-image" />
